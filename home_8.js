ce0f4f5052e419be5fea678a05f27ad158a3a6d3
JuliiaBurenko <ju.burenko@gmail.com>
2024-03-30 22:14:46 +0100

    Commit 30.03.2024

---
+++ home_8.js
@@ -0,0 +1,69 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+
+
+
+function findValueByKey(companyName){
+    if (company.name===companyName) {
+        return company
+    }
+    if (!company.clients) {
+        return null;
+    }
+    let subCompArr = company.clients
+    while (subCompArr.length>0) {
+        const nextCompany=subCompArr.pop();
+        if (nextCompany.name===companyName) {
+            return nextCompany;
+        }
+
+        if (nextCompany.partners && nextCompany.partners.length>0) {
+            subCompArr = subCompArr.concat(nextCompany.partners);
+        }
+    }
+    return null;
+}
+
+const result= findValueByKey('Велика Компанія');
+console.log(result);
